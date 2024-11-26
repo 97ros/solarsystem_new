c314d9ce4149c8cf7d0a3619ffd52a07f9818a04
StivenC03 <s.cfarku@studenti.poliba.it>
2024-11-26 09:59:37 +0100

    Better Horizontal movement
    Reduce the horizontal movement of the spaceship for better control

--- solarsystem/Assets/Scripts/Spaceship Movement/PlayerSpaceship.cs
+++ solarsystem/Assets/Scripts/Spaceship Movement/PlayerSpaceship.cs
@@ -8,16 +8,18 @@ public class PlayerSpaceship : MonoBehaviour
 
     Rigidbody spaceshipRB;
 
-    //Inputs
+    // Inputs
     float verticalMove;
     float horizontalMove;
     float mouseInputX;
     float mouseInputY;
     float rollInput;
 
-    //Speed Multipliers
+    // Speed Multipliers
     [SerializeField]
     float speedMult = 1;
+    [SerializeField]
+    float horizontalSpeedMult = 0.5f; // Moltiplicatore per lo spostamento orizzontale
     [SerializeField]
     float speedMultAngle = 0.5f;
     [SerializeField]
@@ -58,7 +60,7 @@ public class PlayerSpaceship : MonoBehaviour
     void FixedUpdate()
     {
         spaceshipRB.AddForce(spaceshipRB.transform.TransformDirection(Vector3.forward) * verticalMove * speedMult, ForceMode.VelocityChange);
-        spaceshipRB.AddForce(spaceshipRB.transform.TransformDirection(Vector3.right) * horizontalMove * speedMult, ForceMode.VelocityChange);
+        spaceshipRB.AddForce(spaceshipRB.transform.TransformDirection(Vector3.right) * horizontalMove * horizontalSpeedMult, ForceMode.VelocityChange);
         spaceshipRB.AddTorque(spaceshipRB.transform.right * speedMultAngle * mouseInputY * -1, ForceMode.VelocityChange);
         spaceshipRB.AddTorque(spaceshipRB.transform.up * speedMultAngle * mouseInputX, ForceMode.VelocityChange);
         spaceshipRB.AddTorque(spaceshipRB.transform.forward * speedRollMultAngle * rollInput, ForceMode.VelocityChange);

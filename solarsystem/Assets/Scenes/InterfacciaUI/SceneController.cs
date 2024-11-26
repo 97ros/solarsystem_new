2c4f2b08341c78ccc22b3c73feb6b8b99dac6bb9
auroraaelia <a.elia7@studenti.poliba.it>
2024-11-26 16:54:33 +0100

    script tablet 2

--- solarsystem/Assets/Scenes/InterfacciaUI/SceneController.cs
+++ solarsystem/Assets/Scenes/InterfacciaUI/SceneController.cs
@@ -6,7 +6,7 @@ public class SceneController : MonoBehaviour
     void Start()
     {
         // Carica entrambe le scene additive
-        SceneManager.LoadScene("SistemaSolare", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Tablet", LoadSceneMode.Additive);
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("TabletUI", LoadSceneMode.Additive);
     }
 }

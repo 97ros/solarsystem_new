95b1adb92d80e291df64e21435147b6f8c1fc19d
Rossana <r.lasorsa@studenti.poliba.it>
2024-12-04 10:41:55 +0100

    Interfaccia

--- solarsystem/Assets/Scenes/InterfacciaUI/Tablet/Informazioni/PlanetUIManager.cs
+++ solarsystem/Assets/Scenes/InterfacciaUI/Tablet/Informazioni/PlanetUIManager.cs
@@ -2,22 +2,46 @@ using UnityEngine;
 
 public class PlanetUIManager : MonoBehaviour
 {
-    // Array dei Canvas dei pianeti
-    public GameObject[] planetCanvases;
+    // Riferimenti agli oggetti Stratificazione
+    public GameObject StratificazioneMercurio;
+    public GameObject StratificazioneVenere;
+    public GameObject StratificazioneTerra;
+    public GameObject StratificazioneMarte;
+    public GameObject StratificazioneGiove;
+    public GameObject StratificazioneSaturno;
+    public GameObject StratificazioneUrano;
+    public GameObject StratificazioneNettuno;
 
-    // Funzione per attivare il Canvas corretto
-    public void ShowPlanetCanvas(int planetIndex)
+    // Riferimento a UIinformazioni
+    public GameObject UIinformazioni;
+
+    // Metodo per attivare un oggetto e disattivare tutti gli altri
+    public void ShowStratificazione(GameObject stratificazioneDaAttivare)
     {
-        // Disattiva tutti i Canvas
-        foreach (GameObject canvas in planetCanvases)
-        {
-            canvas.SetActive(false);
-        }
+        // Disattiva tutti gli oggetti di stratificazione
+        StratificazioneMercurio.SetActive(false);
+        StratificazioneVenere.SetActive(false);
+        StratificazioneTerra.SetActive(false);
+        StratificazioneMarte.SetActive(false);
+        StratificazioneGiove.SetActive(false);
+        StratificazioneSaturno.SetActive(false);
+        StratificazioneUrano.SetActive(false);
+        StratificazioneNettuno.SetActive(false);
 
-        // Attiva il Canvas selezionato
-        if (planetIndex >= 0 && planetIndex < planetCanvases.Length)
-        {
-            planetCanvases[planetIndex].SetActive(true);
-        }
+        // Disattiva UIinformazioni
+        UIinformazioni.SetActive(false);
+
+        // Attiva solo l'oggetto passato come parametro
+        stratificazioneDaAttivare.SetActive(true);
     }
+
+    // Questi metodi vengono collegati ai bottoni nell'Inspector
+    public void OnButtonMercurioClick() => ShowStratificazione(StratificazioneMercurio);
+    public void OnButtonVenereClick() => ShowStratificazione(StratificazioneVenere);
+    public void OnButtonTerraClick() => ShowStratificazione(StratificazioneTerra);
+    public void OnButtonMarteClick() => ShowStratificazione(StratificazioneMarte);
+    public void OnButtonGioveClick() => ShowStratificazione(StratificazioneGiove);
+    public void OnButtonSaturnoClick() => ShowStratificazione(StratificazioneSaturno);
+    public void OnButtonUranoClick() => ShowStratificazione(StratificazioneUrano);
+    public void OnButtonNettunoClick() => ShowStratificazione(StratificazioneNettuno);
 }
